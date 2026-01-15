c5dbfe87702d0da8f14745fed405298bed63c0ec
kielllll <eatarranza@gmail.com>
2026-01-15 13:41:56 +0800

    chore: add CORS config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || [
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
